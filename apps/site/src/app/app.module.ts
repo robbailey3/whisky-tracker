6f45e5a22f7dea609f73e5ab83c3392069cde6e0
Rob Bailey <rob.bailey3@gmail.com>
2021-01-14 20:05:34 +0000

    build: adding sentry for error reporting

--- apps/site/src/app/app.module.ts
+++ apps/site/src/app/app.module.ts
@@ -2,15 +2,34 @@
 import { AppRoutingModule } from './app-routing.module';
 import { UiComponentsModule } from '@whisky-tracker/ui-components';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { GlobalModule } from './global/global.module';
+import { Router } from '@angular/router';
+import { NgModule, APP_INITIALIZER, ErrorHandler } from '@angular/core';
+import * as Sentry from '@sentry/angular';
 
+import { GlobalModule } from './global/global.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [AppRoutingModule, BrowserModule, GlobalModule, UiComponentsModule],
-  providers: [],
+  providers: [
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler({
+        showDialog: true
+      })
+    },
+    {
+      provide: Sentry.TraceService,
+      deps: [Router]
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => {},
+      deps: [Sentry.TraceService],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- apps/site/src/main.ts
+++ apps/site/src/main.ts
@@ -1,8 +1,27 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import * as Sentry from '@sentry/angular';
+import { Integrations } from '@sentry/tracing';
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+Sentry.init({
+  dsn:
+    'https://ef0d8e6c01c74a1080ff3636a5da30c7@o368150.ingest.sentry.io/5593172',
+  autoSessionTracking: true,
+  integrations: [
+    new Integrations.BrowserTracing({
+      tracingOrigins: ['localhost'],
+      routingInstrumentation: Sentry.routingInstrumentation
+    })
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0
+});
 
 if (environment.production) {
   enableProdMode();
