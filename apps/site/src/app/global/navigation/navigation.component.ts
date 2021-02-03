0e842293e27cb5d206b1f615826109d7fb132d7f
Rob Bailey <rob.bailey3@gmail.com>
2021-02-03 07:54:25 +0000

    build: working on nav component

--- apps/site/src/app/global/navigation/navigation.component.ts
+++ apps/site/src/app/global/navigation/navigation.component.ts
@@ -1,28 +1,37 @@
-import { Component, HostListener, Inject, OnInit } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  Inject,
+  AfterViewChecked,
+  ViewEncapsulation
+} from '@angular/core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { WINDOW } from '@ng-toolkit/universal';
 import { Subject } from 'rxjs';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { distinctUntilChanged, throttleTime } from 'rxjs/operators';
 
 @Component({
   selector: 'rob-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class NavigationComponent implements OnInit {
-  public isMobile = false;
+export class NavigationComponent implements AfterViewChecked {
+  public isMobile: boolean;
 
   public $windowSize: Subject<number> = new Subject();
 
-  public state: 'open' | 'closed' = 'open';
+  public state: 'open' | 'closed' = 'closed';
 
   public faBars = faBars;
 
   constructor(@Inject(WINDOW) public window: Window) {}
 
-  public ngOnInit() {
-    this.handleWindowResize();
+  public ngAfterViewChecked() {
     this.subscribeToWindowSize();
+    setTimeout(() => {
+      this.handleWindowResize();
+    }, 0);
   }
 
   @HostListener('window:resize')
@@ -31,10 +40,14 @@ export class NavigationComponent implements OnInit {
   }
 
   public subscribeToWindowSize() {
-    this.$windowSize.pipe(debounceTime(200), distinctUntilChanged()).subscribe({
+    this.$windowSize.pipe(throttleTime(200), distinctUntilChanged()).subscribe({
       next: (size: number) => {
         this.isMobile = size < 768;
       }
     });
+  }
+
+  public toggleMenuState() {
+    this.state = this.state === 'open' ? 'closed' : 'open';
   }
 }
